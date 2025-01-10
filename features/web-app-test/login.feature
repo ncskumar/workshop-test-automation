c70c9f6c05a4c57b624a77200c3ee9415d99bb30
Santhosh Kumar N <santhosh.kumar@numocity.com>
2025-01-10 10:19:58 +0530

    updated

--- features/web-app-test/login.feature
+++ features/web-app-test/login.feature
@@ -1,10 +1,10 @@
-Feature: Login functionality
-To verify that the login functionality works as expected
-As a user
-I want to be able to log in with valid credentials
+# Feature: Login functionality
+# To verify that the login functionality works as expected
+# As a user
+# I want to be able to log in with valid credentials
 
-Scenario: Successful login with valid credentials
-Given I am on the login page
-When I enter "admin" and "password" as credentials
-And I click the login button
-Then I should see the homepage
+# Scenario: Successful login with valid credentials
+# Given I am on the login page
+# When I enter "admin" and "password" as credentials
+# And I click the login button
+# Then I should see the homepage
--- features/web-app-test/login_steps.js
+++ features/web-app-test/login_steps.js
@@ -1,38 +1,38 @@
-import { Given, When, Then } from '@cucumber/cucumber';
-import { Builder, By, until } from 'selenium-webdriver';
-import assert from 'assert';
+// import { Given, When, Then } from '@cucumber/cucumber';
+// import { Builder, By, until } from 'selenium-webdriver';
+// import assert from 'assert';
 
-let driver;
+// let driver;
 
-Given('I am on the login page', async function () {
-    driver = new Builder().forBrowser('chrome').build();
-    await driver.get('http://localhost:8085/login.php'); // Replace with your login URL
-});
+// Given('I am on the login page', async function () {
+//     driver = new Builder().forBrowser('chrome').build();
+//     await driver.get('http://localhost:8085/login.php'); // Replace with your login URL
+// });
 
-When('I enter {string} and {string} as credentials', async function (email, password) {
-    const emailField = await driver.findElement(By.name('username')); // Replace with actual ID
-    const passwordField = await driver.findElement(By.name('password')); // Replace with actual ID
+// When('I enter {string} and {string} as credentials', async function (email, password) {
+//     const emailField = await driver.findElement(By.name('username')); // Replace with actual ID
+//     const passwordField = await driver.findElement(By.name('password')); // Replace with actual ID
 
-    await emailField.sendKeys(email);
-    await passwordField.sendKeys(password);
-});
+//     await emailField.sendKeys(email);
+//     await passwordField.sendKeys(password);
+// });
 
-When('I click the login button', async function () {
-    const loginButton = await driver.findElement(By.name('Login')); // Replace with actual ID
-    await loginButton.click();
-});
+// When('I click the login button', async function () {
+//     const loginButton = await driver.findElement(By.name('Login')); // Replace with actual ID
+//     await loginButton.click();
+// });
 
-Then('I should see the homepage', async function () {
-    //const title = await driver.getTitle();
-   const header =  await driver.wait(async () => {
-        const title = await driver.getTitle();
-        return title; // Replace with your condition
-    }, 5000);
+// Then('I should see the homepage', async function () {
+//     //const title = await driver.getTitle();
+//    const header =  await driver.wait(async () => {
+//         const title = await driver.getTitle();
+//         return title; // Replace with your condition
+//     }, 5000);
 
-    if (header === 'Welcome :: Damn Vulnerable Web Application (DVWA) v1.10 *Development*') { // Replace 'Dashboard' with the expected title
-        console.log('Dashboard is loaded');
-    } else {
-        console.log('Dashboard title not found');
-    }
-    await driver.quit();
-});
+//     if (header === 'Welcome :: Damn Vulnerable Web Application (DVWA) v1.10 *Development*') { // Replace 'Dashboard' with the expected title
+//         console.log('Dashboard is loaded');
+//     } else {
+//         console.log('Dashboard title not found');
+//     }
+//     await driver.quit();
+// });
